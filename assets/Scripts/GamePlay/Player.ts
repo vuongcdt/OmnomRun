9ae724ae19ada9a9191a238b73dbf5ae2d07b0c6
vuongcdt <vuongcdt@gmail.com>
2024-11-06 23:27:19 +0700

    update easing changeLane

--- assets/Scripts/GamePlay/Player.ts
+++ assets/Scripts/GamePlay/Player.ts
@@ -1,4 +1,4 @@
-import { _decorator, Camera, CapsuleCollider, Component, game, ICollisionEvent, Input, input, KeyCode, Node, RigidBody, Vec3 } from 'cc';
+import { _decorator, Camera, CapsuleCollider, Component, game, ICollisionEvent, Input, input, KeyCode, MeshRenderer, Node, RigidBody, tween, Vec3 } from 'cc';
 import { LaneRoad } from '../Common/Enums';
 import { eventTarget, PATH_SPAWNER } from './Events';
 const { ccclass, property } = _decorator;
@@ -22,7 +22,10 @@ export class Player extends Component {
     private _rgAvatar: RigidBody;
     private _avatar: Node;
     private _capsuleCollier: CapsuleCollider;
-    private _cameraPos: Vec3 = new Vec3();
+    private _offsetCameraPos: Vec3 = new Vec3();
+    private _image: Node;
+    private _playerPos: Vec3 = new Vec3();
+    private _avatarPos: Vec3 = new Vec3();
 
     start() {
         input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
@@ -31,8 +34,10 @@ export class Player extends Component {
         this._capsuleCollier.on('onCollisionExit', this.onCollisionExit, this);
         this._rgAvatar = this.getComponentInChildren(RigidBody);
         this._avatar = this._rgAvatar.node;
+        this._image = this.getComponentInChildren(MeshRenderer).node;
         this._rgAvatar.applyImpulse(Vec3.FORWARD);
-        this._cameraPos = this.camera.node.position.clone();
+        this._offsetCameraPos = this.camera.node.position.clone();
+        this._playerPos = this.node.position;
     }
 
     onDestroy() {
@@ -70,9 +75,16 @@ export class Player extends Component {
 
     private changeLane(direction: number) {
         this._currentLane += direction;
+
+        if (this._currentLane >= 0 && this._currentLane <= 2) {
+            this._image.setPosition(new Vec3(-2 * direction, 0));
+            tween(this._image)
+                .to(0.4, { position: new Vec3(0, 0) }, { easing: 'quadOut' })
+                .start();
+        }
         this._currentLane = Math.max(0, Math.min(2, this._currentLane));
 
-        const targetPosition = new Vec3(this._currentLane * this.laneDistance - this.laneDistance, this.node.position.y, this.node.position.z);
+        const targetPosition = new Vec3(this._currentLane * this.laneDistance - this.laneDistance, this._playerPos.y, this._playerPos.z);
         this.node.setPosition(targetPosition);
     }
 
@@ -86,7 +98,11 @@ export class Player extends Component {
     }
 
     private slide() {
-        if (this._isSliding || this._isJumping) {
+        if (this._isSliding) {
+            return;
+        }
+        if (this._isJumping) {
+
             return;
         }
 
@@ -100,12 +116,11 @@ export class Player extends Component {
     }
 
     update(deltaTime: number) {
-        // return;
         this._avatar.angle = 0;
         const newPlayerPos = new Vec3(0, 0, -this.forwardSpeed * deltaTime);
         this.node.translate(newPlayerPos);
-        const avatarPos = this._avatar.position;
-        const newCamPos = new Vec3(this.node.position.x / 8, avatarPos.y + this._cameraPos.y, this.node.position.z + this._cameraPos.z);
+        this._avatarPos = this._avatar.position;
+        const newCamPos = new Vec3(this._playerPos.x + this._image.position.x, this._avatarPos.y + this._offsetCameraPos.y, this._playerPos.z + this._offsetCameraPos.z);
         this.camera.node.setPosition(newCamPos);
     }
 
@@ -123,7 +138,7 @@ export class Player extends Component {
         const contactPoint = new Vec3();
         contacts[0].getWorldPointOnA(contactPoint);
 
-        if (contactPoint.y >= this._avatar.position.y - this._capsuleCollier.cylinderHeight * 0.5) {
+        if (contactPoint.y >= this._avatarPos.y - this._capsuleCollier.cylinderHeight * 0.5) {
             game.pause();
             console.log('game over');
             return
