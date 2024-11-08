d3a21a132628d466b71a9cb154ce90e7cd8621f4
vuongcdt <vuongcdt@gmail.com>
2024-11-08 14:41:57 +0700

    add redirect player

---
+++ assets/Scripts/Common/Utils.ts
@@ -0,0 +1,30 @@
+import { math, Quat, v3, Vec3 } from "cc";
+
+
+export const rotatePointAroundY = (point: Vec3, angle: number): Vec3 => {
+    const radian = math.toRadian(angle);
+
+    const x = point.x * Math.cos(radian) + point.z * Math.sin(radian);
+    const z = -point.x * Math.sin(radian) + point.z * Math.cos(radian);
+
+    return new Vec3(x, point.y, z).subtract(v3(point.x, point.y));
+}
+
+export const rotatePointAroundCenter = (point: Vec3, center: Vec3, angle: number): Vec3 => {
+    const radian = math.toRadian(angle);
+
+    const translatedPoint = new Vec3(point.x - center.x, point.y - center.y, point.z - center.z);
+
+    const x = translatedPoint.x * Math.cos(radian) + translatedPoint.z * Math.sin(radian);
+    const z = -translatedPoint.x * Math.sin(radian) + translatedPoint.z * Math.cos(radian);
+
+    return new Vec3(x + center.x, translatedPoint.y + center.y, z + center.z);
+}
+
+export const angleToQuaternion = (angle: number, axis: Vec3): Quat => {
+    const radian = math.toRadian(angle);
+    const quat = new Quat();
+    Quat.fromAxisAngle(quat, axis, radian);
+
+    return quat;
+}
--- assets/Scripts/GamePlay/Player.ts
+++ assets/Scripts/GamePlay/Player.ts
@@ -1,7 +1,8 @@
-import { _decorator, CapsuleCollider, Component, game, ICollisionEvent, Input, input, ITriggerEvent, KeyCode, Node, RigidBody, Vec3 } from 'cc';
+import { _decorator, CapsuleCollider, Component, game, ICollisionEvent, Input, input, ITriggerEvent, KeyCode, math, Node, Quat, RigidBody, v3, Vec3 } from 'cc';
 import { LaneRoad } from '../Common/Enums';
 import { eventTarget, PATH_SPAWNER } from './Events';
 import { Collectable } from './Collectable';
+import { angleToQuaternion, rotatePointAroundCenter } from '../Common/Utils';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
@@ -14,6 +15,8 @@ export class Player extends Component {
     private jumpHeight: number = 10;
     @property
     private slideTime: number = 1;
+    @property
+    private speedRedirect: number = 1;
 
     private _targetLane: LaneRoad = LaneRoad.MidlleLane;
     private _isJumping: boolean = false;
@@ -22,6 +25,12 @@ export class Player extends Component {
     private _playerAction: Node;
     private _capsuleCollier: CapsuleCollider;
     private _playerPos: Vec3 = new Vec3();
+
+    private _distance: number = -30;
+
+    private _point: Vec3 = new Vec3(0, 0, this._distance);
+    private _angle: number = 0;
+    private _center: Vec3 = new Vec3(-5, 0, this._distance);
 
     start() {
         input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
@@ -38,70 +47,25 @@ export class Player extends Component {
         input.off(Input.EventType.KEY_DOWN, this.onKeyDown, this);
     }
 
-    private onKeyDown(event: any) {
-        switch (event.keyCode) {
-            case KeyCode.KEY_A:
-                this.changeLane(-1);
-                break;
-            case KeyCode.KEY_D:
-                this.changeLane(1);
-                break;
-            case KeyCode.KEY_W:
-                this.jump();
-                break;
-            case KeyCode.KEY_S:
-                this.slide();
-                break;
-            case KeyCode.ARROW_LEFT:
-                this.changeLane(-1);
-                break;
-            case KeyCode.ARROW_RIGHT:
-                this.changeLane(1);
-                break;
-            case KeyCode.ARROW_UP:
-                this.jump();
-                break;
-            case KeyCode.ARROW_DOWN:
-                this.slide();
-                break;
-        }
-    }
 
-    private changeLane(direction: number) {
-        this._targetLane += direction;
-
-        this._targetLane = Math.max(0, Math.min(2, this._targetLane));
-    }
-
-    private jump() {
-        if (this._isJumping) {
+    private redirect() {
+        this._angle += this.speedRedirect;
+        if (this._angle > 90) {
+            game.pause()
             return;
         }
 
-        this._isJumping = true;
-        this._rgPlayerAction.applyImpulse(new Vec3(0, this.jumpHeight));
-    }
-
-    private slide() {
-        if (this._isSliding) {
-            return;
-        }
-
-        if (this._isJumping) {
-            this._rgPlayerAction.applyImpulse(new Vec3(0, -this.jumpHeight * 1.3));
-            return;
-        }
-
-        this._isSliding = true;
-        this._capsuleCollier.cylinderHeight = 0;
-
-        setTimeout(() => {
-            this._capsuleCollier.cylinderHeight = 1;
-            this._isSliding = false;
-        }, this.slideTime * 1000);
+        const rotatedPoint = rotatePointAroundCenter(this._point, this._center, this._angle);
+        this.node.position = rotatedPoint;
+        this.node.rotate(angleToQuaternion(this.speedRedirect, Vec3.UNIT_Y));
     }
 
     update(deltaTime: number) {
+        if (this._playerPos.z < this._distance) {
+            this.redirect();
+            return;
+        }
+
         this._playerAction.angle = 0;
         this.node.angle = 0;
         this._playerAction.position.subtract3f(0, 0, this._playerAction.position.z);
@@ -143,5 +107,61 @@ export class Player extends Component {
         }
 
         this._isJumping = false;
+    }
+
+
+    private onKeyDown(event: any) {
+        switch (event.keyCode) {
+            case KeyCode.KEY_A:
+            case KeyCode.ARROW_LEFT:
+                this.changeLane(-1);
+                break;
+            case KeyCode.KEY_D:
+            case KeyCode.ARROW_RIGHT:
+                this.changeLane(1);
+                break;
+            case KeyCode.KEY_W:
+            case KeyCode.ARROW_UP:
+                this.jump();
+                break;
+            case KeyCode.KEY_S:
+            case KeyCode.ARROW_DOWN:
+                this.slide();
+                break;
+        }
+    }
+
+    private changeLane(direction: number) {
+        this._targetLane += direction;
+
+        this._targetLane = Math.max(0, Math.min(2, this._targetLane));
+    }
+
+    private jump() {
+        if (this._isJumping) {
+            return;
+        }
+
+        this._isJumping = true;
+        this._rgPlayerAction.applyImpulse(new Vec3(0, this.jumpHeight));
+    }
+
+    private slide() {
+        if (this._isSliding) {
+            return;
+        }
+
+        if (this._isJumping) {
+            this._rgPlayerAction.applyImpulse(new Vec3(0, -this.jumpHeight * 1.3));
+            return;
+        }
+
+        this._isSliding = true;
+        this._capsuleCollier.cylinderHeight = 0;
+
+        setTimeout(() => {
+            this._capsuleCollier.cylinderHeight = 1;
+            this._isSliding = false;
+        }, this.slideTime * 1000);
     }
 }
